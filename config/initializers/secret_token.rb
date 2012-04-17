f4d26169a586241daf96f742020ccf87ddb1e1e6
Marcelo Cajueiro <marcelocajueiro@gmail.com>
2012-04-17 20:37:24 -0300

    Initial import of a new rails project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaskValidatorExample::Application.config.secret_token = '7442de500fd386a8c0328bb6f617fef5856966a80f2bbf13e123262236ff2fdcf96c97a7c08bf297a21b3891bcfee95c37e48645b7d14f91ec2c1f306fcd3af1'
